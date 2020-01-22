d91051d1871d64e9a70887fed88177a09b59ac96
Adrian Guadalupe <safisweetkeyz@gmail.com>
2020-01-22 13:52:57 -0500

    dark mode working

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 export const useLocalStorage = (key, initialValue) => {
    const [storedValue, setStoredValue] = useState(() => {
       const item = window.localStorage.getItem(key);
